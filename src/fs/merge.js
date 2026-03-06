b5d1aeee37abdc2146548a4b341e52aa140b378b
Ir4D <iradedova@gmail.com>
2026-03-06 13:29:08 +0200

    feat: add fs:merge task

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -1,8 +1,58 @@
+import { promises as fsPromises } from "node:fs";
+import path from "node:path";
+import { argv } from "node:process";
+
 const merge = async () => {
-  // Write your code here
-  // Default: read all .txt files from workspace/parts in alphabetical order
-  // Optional: support --files filename1,filename2,... to merge specific files in provided order
-  // Concatenate content and write to workspace/merged.txt
+  try {
+    const folder = path.join("./", "workspace", "parts");
+    const files = await fsPromises.readdir(folder, {
+      withFileTypes: true,
+      recursive: true,
+    });
+
+    const filesArr = [];
+    let specFiles = null;
+
+    if (argv.includes("--files")) {
+      specFiles = argv[argv.length - 1];
+      specFiles = specFiles.split(",");
+      filesArr.push(...specFiles);
+
+      for (const file of filesArr) {
+        try {
+          await fsPromises.access(path.join(folder, file));
+        } catch {
+          throw new Error("FS operation failed");
+        }
+      }
+    } else {
+      for (const file of files) {
+        if (file.name.endsWith("txt")) {
+          filesArr.push(file.name);
+        }
+      }
+      if (filesArr.length === 0) {
+        throw new Error("FS operation failed");
+      }
+    }
+
+    filesArr.sort();
+    let data = "";
+
+    for (const file of filesArr) {
+      const filePath = path.join(folder, file);
+      const contents = await fsPromises.readFile(filePath, {encoding: "utf8",});
+      data += " " + contents;
+    }
+
+    const mergedFilePath = path.join("./", "workspace", "merged.txt");
+    await fsPromises.writeFile(mergedFilePath, data);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
 };
 
 await merge();
