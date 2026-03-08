0ef18366fd2d9edb6ab4e3cfab443b031ae0d105
Ir4D <iradedova@gmail.com>
2026-03-08 12:04:04 +0200

    feat: add modules:dynamic task

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,30 @@
+import { promises as fsPromises } from "node:fs";
+import path from "node:path";
+import { argv } from "node:process";
+import { pathToFileURL } from "node:url";
+
 const dynamic = async () => {
-  // Write your code here
-  // Accept plugin name as CLI argument
-  // Dynamically import plugin from plugins/ directory
-  // Call run() function and print result
-  // Handle missing plugin case
+  try {
+    const folder = path.join("./", "src", "modules", "plugins");
+    const specFile = argv[argv.length - 1];
+    let filePath;
+    if (specFile.includes(".js")) {
+      filePath = path.join(folder, specFile);
+    } else {
+      filePath = path.join(folder, `${specFile}.js`);
+    }
+    const fullPath = pathToFileURL(filePath).href;
+    try {
+      await fsPromises.access(filePath);
+      const module = await import(fullPath);
+      console.log(module.run());
+    } catch {
+      console.log("Plugin not found");
+      process.exit(1);
+    }
+  } catch (err) {
+    throw err;
+  }
 };
 
 await dynamic();
